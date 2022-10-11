56f16c8581b5b7507eab65f79b974b26789090f5
William van Beek <wvanbeek@precima.com>
2022-10-11 11:44:10 +0200

    Added 2 merge requests

--- src/MicrosoftTeamsNotification.groovy
+++ src/MicrosoftTeamsNotification.groovy
@@ -16,7 +16,7 @@ rundeckPlugin(NotificationPlugin){
         json_payload = JsonOutput.toJson([
             title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
             summary: "Rundeck Job Notification",
-            text: "$job id: #${execution.job.id}, job project: ${execution.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}",
+            text: "job id: #${execution.job.id}, job project: ${execution.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}",
             themeColor: "${color}",
             potentialAction: [
                 [
@@ -54,13 +54,35 @@ rundeckPlugin(NotificationPlugin){
 
         return true
     }
+    
+    onretryablefailure {
+        type = "RETRY"
+        color = "FFB900"
+        //Single argument, the configuration properties are available automatically
+        json_payload = JsonOutput.toJson([
+            title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
+            summary: "Rundeck Job Notification",
+            text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
+            themeColor: "${color}",
+            potentialAction: [
+                [
+                    "@context": "http://schema.org",
+                    "@type": "ViewAction",
+                    name: "Seed job execution",
+                    target: ["${execution.href}"]
+                ]
+            ]
+        ])
+        process = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${json_payload}' ${configuration.webhook_url}" ].execute().text
+
+        return true
+    }
 
     onsuccess {
         type = "SUCCESS"
         color = "228B22"
         //with no args, there is a "configuration" and an "execution" variable in the context
         json_payload = JsonOutput.toJson([
-            title: "Rundeck Job Notification",
             title: "[${type}] Rundeck Job Notification - ${execution.project}:${execution.job.group}:${execution.job.name}",
             text: "job id: #${execution.job.id}, job project: ${execution.job.project}, job group: ${execution.job.group}, job name: ${execution.job.name}, job description: ${execution.job.description}, execution id: #${execution.id}, execution status: ${execution.status}, execution started at: ${execution.dateStarted}, execution ended at: ${execution.dateEnded}",
             themeColor: "${color}",
